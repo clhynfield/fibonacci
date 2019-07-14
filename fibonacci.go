1c4d112bb0812d3f834b0528009ba5963083b0d7
Clayton Hynfield <clayton@hynfield.org>
2019-07-14 10:05:21 -0400

    Set up test-driving Fibonacci package with Ginkgo
    ```
    go get github.com/onsi/ginkgo/ginkgo
    go get github.com/onsi/ginkgo/extensions/table
    go get github.com/onsi/gomega
    
    ginkgo bootstrap
    ginkgo generate fibonacci
    ```

---
+++ fibonacci.go
@@ -0,0 +1,7 @@
+package fibonacci
+
+type Fibonacci struct{}
+
+func (f Fibonacci) Fib(steps int) (result int) {
+	return
+}
---
+++ fibonacci_test.go
@@ -0,0 +1,19 @@
+package fibonacci_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	. "github.com/clhynfield/fibonacci"
+)
+
+var _ = Describe("Fibonacci", func() {
+	var fibonacci Fibonacci
+
+	DescribeTable("Calculating Fibonacci numbers",
+		func(steps int, expected int) {
+			Expect(fibonacci.Fib(steps)).To(Equal(expected))
+		},
+	)
+})
