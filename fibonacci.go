c365cb181e05ba65d5803044adccd25b8bc4967c
Clayton Hynfield <clayton@hynfield.org>
2019-07-15 10:22:43 -0400

    Refactor Fib() steps 3-5
    Notice the pattern?

--- fibonacci.go
+++ fibonacci.go
@@ -8,11 +8,11 @@ func (f Fibonacci) Fib(steps int) (result int) {
 	if steps == 1 || steps == 2 {
 		result = 1
 	} else if steps == 3 {
-		result = 2
+		result = f.Fib(2) + f.Fib(1)
 	} else if steps == 4 {
-		result = 3
+		result = f.Fib(3) + f.Fib(2)
 	} else if steps == 5 {
-		result = 5
+		result = f.Fib(4) + f.Fib(3)
 	}
 	return result
 }
